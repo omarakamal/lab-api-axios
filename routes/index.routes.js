3b9f707940768b5ea994b91794ff778251fcca65
omarakamal <54825038+omarakamal@users.noreply.github.com>
2023-07-10 13:11:19 +0400

    Add files via upload

---
+++ routes/index.routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios')
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+//ITERATION 1
+//GET route for searching for 1 character
+router.get('/search',(req,res)=>{
+  //Write code here
+})
+
+
+//ITERATION 2
+//POST route for deleting 1 character
+router.post('/search/delete',(req,res)=>{
+
+})
+
+//ITERATION 3
+//POST route for creating a new character
+router.post('/search/new-character',(req,res)=>{
+
+})
+
+
+//ITERATION 4
+//POST route for editing 1 character
+router.post('/search/edit',(req,res)=>{
+ 
+})
+
+module.exports = router;
