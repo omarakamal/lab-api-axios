bfc09d0603d3b820981f1d7df60e48a74f4b8496
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-07-22 10:41:11 +0200

    formatting the routes to have the same pattern

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,36 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const axios = require('axios')
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+router.get('/', (req, res, next) => {
+	res.render('index')
+})
 
 //ITERATION 1
 //GET route for searching for 1 character
-router.get('/search',(req,res)=>{
-  //Write code here
+router.get('/search', (req, res) => {
+	//Write code here
 })
-
 
 //ITERATION 2
 //POST route for deleting 1 character
-router.post('/search/delete',(req,res)=>{
-
+router.post('/search/delete', (req, res) => {
+  //Write code here
 })
 
 //ITERATION 3
 //POST route for creating a new character
-router.post('/search/new-character',(req,res)=>{
-
+router.post('/search/new-character', (req, res) => {
+  //Write code here
 })
-
 
 //ITERATION 4
 //POST route for editing 1 character
-router.post('/search/edit',(req,res)=>{
- 
+router.post('/search/edit', (req, res) => {
+  //Write code here
 })
 
-module.exports = router;
+module.exports = router
