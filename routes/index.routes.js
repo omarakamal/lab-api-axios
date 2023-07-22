e7a2f052f3b77c792cebabc242c44fc1d2cdbbb6
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-07-22 10:54:38 +0200

    Iteration number matching the README file

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,25 +7,25 @@ router.get('/', (req, res, next) => {
 	res.render('index')
 })
 
-//ITERATION 1
+//ITERATION 4
 //GET route for searching for 1 character
 router.get('/search', (req, res, next) => {
 	//Write code here
 })
 
-//ITERATION 2
+//ITERATION 5
 //POST route for deleting 1 character
 router.post('/search/delete', (req, res, next) => {
   //Write code here
 })
 
-//ITERATION 3
+//ITERATION 6
 //POST route for creating a new character
 router.post('/search/new-character', (req, res, next) => {
   //Write code here
 })
 
-//ITERATION 4
+//ITERATION 7
 //POST route for editing 1 character
 router.post('/search/edit', (req, res, next) => {
   //Write code here
