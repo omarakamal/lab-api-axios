4dc55513fb9fe71038dfc488bf03facf5332235a
Maria Garcia <mariagarciamontes06@outlook.com>
2023-07-25 22:05:22 +0200

    Done iteration 1-4

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -11,13 +11,23 @@ router.get('/', (req, res, next) => {
 //GET route for searching for 1 character
 router.get('/all', (req, res, next) => {
 	//Write code here
-	
+	axios.get('http://localhost:8000/characters')
+	.then((result)=>{
+		res.render('index',{allCharacters:result.data})
+	})
+	.catch((err)=>console.log(err))
 })
 
 //ITERATION 4
 //GET route for searching for 1 character
 router.get('/search', (req, res, next) => {
 	//Write code here
+	const {id} = req.query
+	axios.get(`http://localhost:8000/characters/${id}`)
+	.then((result)=>{
+		res.render('index',{oneCharacter:result.data})
+	})
+	.catch((err)=>console.log(err))
 })
 
 //ITERATION 5
