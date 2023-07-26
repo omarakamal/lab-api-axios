19a475843b66244ac3b5f06d6d37ae9972c3380f
Hadrien MAUVE <mauve.hadrien@gmail.com>
2023-07-26 14:16:28 +0200

    done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -13,34 +13,50 @@ router.get('/all', (req, res, next) => {
 	axios.get('http://localhost:8000/characters')
 	.then((characters) => {
 		res.render('index',{characters:characters.data})
-	})	
+	})
+	.catch(err => console.log(err))	
 })
 
 //GET route for searching for 1 character
 router.get('/search', (req, res, next) => {
-	console.log(req.params)
-	axios.get(`http://localhost:8000/characters/:id`)
+	axios.get(`http://localhost:8000/characters/${req.query.id}`)
 	.then((character) => {
-		res.render('index',{character:character.data})
+		const characters = [character.data]
+		res.render('index',{characters})
 	})
+	.catch(err => console.log(err))
 })
 
 //ITERATION 5
 //POST route for deleting 1 character
 router.post('/search/delete', (req, res, next) => {
-	//Write code here
+	axios.delete(`http://localhost:8000/characters/${req.body.id}`)
+	.then((character) => {
+		res.redirect('/')
+	})
+	.catch(err => console.log(err))
 })
 
 //ITERATION 6
 //POST route for creating a new character
 router.post('/search/new-character', (req, res, next) => {
-	//Write code here
+	const {name, occupation, weapon, cartoon} = req.body
+	axios.post(`http://localhost:8000/characters/`, {name, occupation, weapon, cartoon})
+	.then((character) => {
+		res.redirect('/')
+	})
+	.catch(err => console.log(err))
 })
 
 //ITERATION 7
 //POST route for editing 1 character
 router.post('/search/edit', (req, res, next) => {
-	//Write code here
+	const {name, occupation, weapon, cartoon} = req.body
+	axios.put(`http://localhost:8000/characters/${req.body.id}`, {name, occupation, weapon, cartoon})
+	.then((character) => {
+		res.redirect('/')
+	})
+	.catch(err => console.log(err))
 })
 
 module.exports = router
