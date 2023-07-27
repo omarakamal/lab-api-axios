2864bf7591da2f9941d3c361d7423d4e632654c0
tngknzr <agustinparedes95@gmail.com>
2023-07-27 17:41:05 +0200

    ommmmg2

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,11 +6,15 @@ const axios = require('axios')
 router.get('/', (req, res, next) => {
 	res.render('index')
 })
+
 router.get('/characters', (req, res) => {
 	axios.get('http://localhost:8000/characters')
 	.then(result =>{
 		// console.log(result.data)
 		res.render('characters',result.data)
+	})
+	.catch(err=>{
+		console.log(err)
 	})
 	
 })
@@ -20,6 +24,9 @@ router.get('/all',(req,res) => {
 		console.log(result.data)
 		res.render('all.hbs',{characters:result.data})
 	})
+	.catch(err=>{
+		console.log(err)
+	})
 	
 })
 router.get('/characters/:id',(req,res) =>{
@@ -27,6 +34,9 @@ router.get('/characters/:id',(req,res) =>{
 	axios.get(`http://localhost:8000/characters/${id}`)
 	.then(result =>{
 		res.render('search',{characters:result.data})
+	})
+	.catch(err=>{
+		console.log(err)
 	})
 })
 
@@ -41,6 +51,29 @@ router.post('/search/new-character',(req,res)=>{
 		console.log(err)
 	})
 })
+router.post('/search/:id/delete',(req,res)=>{
+	
+	axios.delete(`http://localhost:8000/characters/${req.params.id}`)
+	.then(()=>{
+		res.redirect('/all')
+	})
+	.catch(err=>{
+		console.log(err)
+	})
+})
+router.get('/search/:id/edit',(req,res)=>{
+	axios.get(`http://localhost:8000/characters/${req.params.id}`)
+	.then((result)=>{
+		res.render('edit-char',{characters:result.data})
+	})
+})
+router.post('/search/:id/edit',(req,res)=>{
+	axios.put(`http://localhost:8000/characters/${req.params.id}`,req.body)
+	.then((result)=>{
+		res.redirect('/all')
+	})
+})
+
 
 
 //ITERATION 3
