dfc68e5c4244ce09f141780075475c93bfd9b432
tngknzr <agustinparedes95@gmail.com>
2023-07-27 14:19:19 +0200

    omg

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,6 +6,42 @@ const axios = require('axios')
 router.get('/', (req, res, next) => {
 	res.render('index')
 })
+router.get('/characters', (req, res) => {
+	axios.get('http://localhost:8000/characters')
+	.then(result =>{
+		// console.log(result.data)
+		res.render('characters',result.data)
+	})
+	
+})
+router.get('/all',(req,res) => {
+	axios.get('http://localhost:8000/characters')
+	.then(result =>{
+		console.log(result.data)
+		res.render('all.hbs',{characters:result.data})
+	})
+	
+})
+router.get('/characters/:id',(req,res) =>{
+	const {id} = req.params
+	axios.get(`http://localhost:8000/characters/${id}`)
+	.then(result =>{
+		res.render('search',{characters:result.data})
+	})
+})
+
+router.post('/search/new-character',(req,res)=>{
+	console.log(req.body)
+	const {name,occupation,weapon} = req.body
+	axios.post('http://localhost:8000/characters',{name,occupation,weapon})
+	.then(()=>{
+		res.redirect('/all')
+	})
+	.catch(err=>{
+		console.log(err)
+	})
+})
+
 
 //ITERATION 3
 //GET route for searching for 1 character
