850ef6ce635608ade1432bf0a9e87c80b940f7be
tngknzr <agustinparedes95@gmail.com>
2023-07-27 17:51:59 +0200

    wannapush

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -76,34 +76,6 @@ router.post('/search/:id/edit',(req,res)=>{
 
 
 
-//ITERATION 3
-//GET route for searching for 1 character
-router.get('/all', (req, res, next) => {
-	//Write code here	
-})
 
-//ITERATION 4
-//GET route for searching for 1 character
-router.get('/search', (req, res, next) => {
-	//Write code here
-})
-
-//ITERATION 5
-//POST route for deleting 1 character
-router.post('/search/delete', (req, res, next) => {
-	//Write code here
-})
-
-//ITERATION 6
-//POST route for creating a new character
-router.post('/search/new-character', (req, res, next) => {
-	//Write code here
-})
-
-//ITERATION 7
-//POST route for editing 1 character
-router.post('/search/edit', (req, res, next) => {
-	//Write code here
-})
 
 module.exports = router
